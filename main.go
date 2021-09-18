5972caa3a2ca3c2a7db8306afb83272e911487e7
jgalvez1776 <jgalvez1776@gmail.com>
2021-09-17 22:15:59 -0700

    Debugging

--- main.go
+++ main.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"fmt"
-
-	game "github.com/JGalvez1776/go-demo/src/game"
-	exportexample "github.com/JGalvez1776/go-demo/src/syntax/export-example"
-	math "github.com/JGalvez1776/go-demo/src/utils"
 )
 
 func main() {
 	// Note semicolons are not part of the source code
 	// they are added during compilation to help with grammar
 	fmt.Println("This is working")
-	fmt.Println(exportexample.Public)
-	fmt.Println(math.Factorial((4)))
-	exportexample.Export()
-	game.Start()
 
 }
