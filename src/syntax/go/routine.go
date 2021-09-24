a415fb35340d1fed0a39d651f0ea004665db1f10
Justin A <jgalvez1776@gmail.com>
2021-09-23 17:44:51 -0700

    gorountine example

--- src/syntax/go/routine.go
+++ src/syntax/go/routine.go
@@ -7,12 +7,13 @@ import (
 
 func Go_routine_example() {
 	channel := make(chan int)
-	for i := 10; i < 30; i = i + 5 {
-		go wait_a_long_time(i, channel)
-		fmt.Println("Here we back in the function that called the wait")
-		<-channel
-		fmt.Printf("We know have %v sent back from waiting", channel)
-	}
+
+	go wait_a_long_time(10, channel)
+	fmt.Println("Here we back in the function that called the wait")
+	<-channel
+	fmt.Printf("We know have %v sent back from waiting", channel)
+	fmt.Println("This function is now done")
+
 }
 
 func wait_a_long_time(time_to_sleep int, channel chan int) {
